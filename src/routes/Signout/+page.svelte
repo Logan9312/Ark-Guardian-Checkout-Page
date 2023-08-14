cd708ef7c6d162c3798fe3f0ae9769bf265fccf2
Logan <lgntravis@gmail.com>
2023-08-14 15:40:03 -0600

    Adding Signout

---
+++ src/routes/Signout/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { onMount } from 'svelte';
+	import { signOut } from '@auth/sveltekit/client';
+	onMount(() => {
+		signOut('discord');
+		window.location.href = '/';
+	});
+</script>
